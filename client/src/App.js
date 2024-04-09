a65d2009b4d0a4fea42c1a93ebc3356bf5e7aa82
Akanksha Singh <akanksha.singh0710@gmail.com>
2024-04-09 01:14:08 -0400

    amplify deployment

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,38 @@
 import './App.css';
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import AWS from 'aws-sdk';
 import { nanoid } from 'nanoid'
-
-
-// Configure AWS credentials and S3 bucket details
-AWS.config.update({
-  accessKeyId: process.env.REACT_APP_accessKeyId,
-  secretAccessKey: process.env.REACT_APP_secretAccessKey,
-  region: process.env.REACT_APP_region,
-});
-
-const S3 = new AWS.S3();
-const BUCKET_NAME = process.env.REACT_APP_BUCKET_NAME;
-const API_GATEWAY_KEY = process.env.REACT_APP_API_GATEWAY_KEY;
-const API_GATEWAY_URL = process.env.REACT_APP_API_GATEWAY_URL + API_GATEWAY_KEY;
+import {
+  GetSecretValueCommand,
+  SecretsManagerClient,
+} from "@aws-sdk/client-secrets-manager";
 
 function App() {
   const [file, setFile] = useState(null);
   const [fileName, setFileName] = useState('');
+
+  useEffect(() => {
+    async function configureAWS() {
+      try {
+        const client = new SecretsManagerClient();
+        const accessKeyId = await client.send(new GetSecretValueCommand({ SecretId: 'secret_access_key' }));
+        const secretAccessKey = await client.send(new GetSecretValueCommand({ SecretId: 'access_key_id' }));
+
+        console.log(accessKeyId);
+        console.log(secretAccessKey);
+        AWS.config.update({
+          accessKeyId: accessKeyId.SecretString,
+          secretAccessKey: secretAccessKey.SecretString,
+          region: process.env.REACT_APP_REGION,
+        });
+
+        console.log('AWS SDK configured successfully');
+      } catch (error) {
+        console.error('Error configuring AWS SDK:', error);
+      }
+    }
+    configureAWS();
+  }, []);
 
   async function handleUpload() {
     if (!file) {
@@ -27,34 +41,31 @@ function App() {
 
     try {
       // Upload the file to S3
+      const s3 = new AWS.S3();
       const s3Params = {
-        Bucket: BUCKET_NAME,
+        Bucket: process.env.REACT_APP_BUCKET_NAME,
         Key: file.name,
         Body: file,
       };
-      await S3.upload(s3Params).promise();
-
-      console.log(s3Params);
+      await s3.upload(s3Params).promise();
 
       // Send the file details to the API Gateway endpoint
       const apiParams = {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
-          'X-Api-Key': API_GATEWAY_KEY,
-
+          'X-Api-Key': process.env.REACT_APP_API_GATEWAY_KEY,
         },
         body: JSON.stringify({
           "TableName": process.env.REACT_APP_TableName,
           "Item": {
             id: nanoid(),
             input_text: fileName,
-            input_file_path: BUCKET_NAME + "/" + file.name
+            input_file_path: `${process.env.REACT_APP_BUCKET_NAME}/${file.name}`
           }
         }),
       };
-      console.log(apiParams);
-      await fetch(API_GATEWAY_URL, apiParams);
+      await fetch(process.env.REACT_APP_API_GATEWAY_URL, apiParams);
 
       alert(`File "${file.name}" uploaded successfully to S3 bucket and stored in DynamoDB.`);
     } catch (error) {
@@ -62,7 +73,6 @@ function App() {
       alert('Failed to upload file.');
     }
   }
-
 
   return (
       <div className="App">
