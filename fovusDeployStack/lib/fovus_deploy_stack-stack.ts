1dfdfe61e7832110e6def3fde665e4fbbd974d94
Akanksha Singh <akanksha.singh0710@gmail.com>
2024-04-09 00:58:34 -0400

    amplify deployment

--- fovusDeployStack/lib/fovus_deploy_stack-stack.ts
+++ fovusDeployStack/lib/fovus_deploy_stack-stack.ts
@@ -1,27 +1,111 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import {Duration} from 'aws-cdk-lib';
+import {Construct} from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
+import {HttpMethods} from 'aws-cdk-lib/aws-s3';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
-import {Duration} from "aws-cdk-lib";
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
-import { App, GitHubSourceCodeProvider } from '@aws-cdk/aws-amplify-alpha'
+import {App, GitHubSourceCodeProvider} from '@aws-cdk/aws-amplify-alpha';
 import {BuildSpec} from "aws-cdk-lib/aws-codebuild";
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
 
 export class DeployFovusStackStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+
+
+    const dynamoDbTable = new dynamodb.Table(this, 'fovus-ddb', {
+      tableName : 'fovus-file-paths',
+      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    const bucket_script = new s3.Bucket(this, 'fovus-scripts-2', {
+      bucketName: 'fovus-scripts-2',
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      enforceSSL: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    const bucket_files = new s3.Bucket(this, 'files-bucket-2', {
+      bucketName: 'files-bucket-2',
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      enforceSSL: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+    bucket_files.addCorsRule({
+      allowedMethods: [ HttpMethods.GET, HttpMethods.POST, HttpMethods.PUT],
+      allowedOrigins: [ '*' ],
+      exposedHeaders: [ 'ETag' ],
+      maxAge: 10000
+    });
+    bucket_script.addCorsRule({
+      allowedMethods: [ HttpMethods.GET],
+      allowedOrigins: [ '*' ],
+      exposedHeaders: [ 'ETag' ],
+      maxAge: 10000
+    });
+
+    new s3deploy.BucketDeployment(this, 'DeployFiles', {
+      sources: [s3deploy.Source.asset('output_file_processor.py.zip')],
+      destinationBucket: bucket_script,
+    });
+
+    const lambdaFunctionDeploy = new lambda.Function(this, 'deployEC2Function', {
+      runtime: lambda.Runtime.NODEJS_LATEST,
+      handler: 'deployEc2.handler',
+      functionName: 'deployEC2',
+      code: lambda.Code.fromAsset('lambdas/ScaleUpAndDownEc2Instance.zip'),
+      timeout: Duration.minutes(5),
+      environment : {
+        TABLE_NAME : dynamoDbTable.tableName,
+      },
+    });
+
+    const lambdaFunctionUpload = new lambda.Function(this, 'uploadFileDetailsFunction', {
+      runtime: lambda.Runtime.NODEJS_LATEST,
+      handler: 'uploadFileDetails.handler',
+      functionName: 'uploadFiles',
+      code: lambda.Code.fromAsset('lambdas/uploadFileDetails.zip'),
+      timeout: Duration.minutes(1)
+    });
+
+    const api = new apigateway.RestApi(this, 'fovus-file-upload-api');
+    const resource = api.root.addResource('uploadFiles');
+    resource.addMethod('POST', new apigateway.LambdaIntegration(lambdaFunctionUpload));
+
+
+    bucket_script.grantReadWrite(lambdaFunctionDeploy);
+    bucket_files.grantReadWrite(lambdaFunctionDeploy);
+    bucket_files.grantPut(lambdaFunctionUpload);
+    bucket_files.grantPut(lambdaFunctionDeploy);
+    bucket_script.grantPut(lambdaFunctionDeploy);
+    dynamoDbTable.grantReadWriteData(lambdaFunctionDeploy);
+
+    const cfn_output = new cdk.CfnOutput(this, "api_url",{
+      value : api.url,
+
+    });
 
     // Create Amplify app
     const amplifyApp = new App(this, 'FovusAmplifyApp', {
       sourceCodeProvider: new GitHubSourceCodeProvider({
         owner: 'akanksha7',
         repository: 'awsStack',
-        oauthToken: cdk.SecretValue.secretsManager('github-token'),
+        oauthToken: cdk.SecretValue.secretsManager('github'),
       }),
       environmentVariables: {
-        AMPLIFY_APP_ROOT: 'FovusChallenge/client',
+        REACT_APP_TableName: dynamoDbTable.tableName,
+        REACT_APP_API_GATEWAY_URL : cfn_output.value,
+        REACT_APP_region :'us-east-1',
+        REACT_APP_BUCKET_NAME: bucket_files.bucketName
       },
+
       buildSpec: BuildSpec.fromObjectToYaml({
         version: "1.0",
         applications: [
@@ -50,58 +134,8 @@ export class DeployFovusStackStack extends cdk.Stack {
     });
     const mainBranch = amplifyApp.addBranch("main");
 
-    const dynamoDbTable = new dynamodb.Table(this, 'fovus-ddb', {
-      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
-      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
-    });
 
-    const bucket_script = new s3.Bucket(this, 'S3Bucket-scripts', {
-      bucketName: 'scripts-bucket',
-      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
-      encryption: s3.BucketEncryption.S3_MANAGED,
-      enforceSSL: true,
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
-    });
 
-    const bucket_files = new s3.Bucket(this, 'S3Bucket-files', {
-      bucketName: 'files-bucket',
-      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
-      encryption: s3.BucketEncryption.S3_MANAGED,
-      enforceSSL: true,
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
-    });
 
-    const lambdaFunctionDeploy = new lambda.Function(this, 'deployEC2Function', {
-      runtime: lambda.Runtime.NODEJS_LATEST,
-      handler: 'deployEc2.handler',
-      functionName: 'deployEC2',
-      code: lambda.Code.fromAsset('lambdas/ScaleUpAndDownEc2Instance.zip'),
-      timeout: Duration.minutes(5),
-      environment : {
-        TABLE_NAME : dynamoDbTable.tableName,
-      },
-    });
-
-    const lambdaFunctionUpload = new lambda.Function(this, 'uploadFileDetailsFunction', {
-      runtime: lambda.Runtime.NODEJS_LATEST,
-      handler: 'uploadFileDetails.handler',
-      functionName: 'uploadFiles',
-      code: lambda.Code.fromAsset('lambdas/uploadFileDetails.zip'),
-      timeout: Duration.minutes(1)
-    });
-
-    const api = new apigateway.RestApi(this, 'fovus-file-upload-api');
-    const resource = api.root.addResource('uploadFiles');
-    resource.addMethod('POST', new apigateway.LambdaIntegration(lambdaFunctionUpload));
-
-    bucket_script.grantReadWrite(lambdaFunctionDeploy);
-    bucket_files.grantReadWrite(lambdaFunctionDeploy);
-    bucket_files.grantPut(lambdaFunctionUpload);
-    //s3Bucket.grantReadWrite(lambdaFunction2);
-
-    new cdk.CfnOutput(this, "HTTP URL",{
-      value : api.url ?? "Error with deployment",
-    });
   }
 }
